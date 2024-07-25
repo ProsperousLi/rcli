ad494537faef4443d50e85d32c13c6758880d4f8
prosperousLi <lirongfan@vrvmail.com.cn>
2024-07-25 15:00:21 +0800

    week2 lesson one: csv to json code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use template::{process_csv, Opts, Subcommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        Subcommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+    Ok(())
 }
---
+++ src/opts.rs
@@ -0,0 +1,38 @@
+// rcli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: Subcommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum Subcommand {
+    #[command(name = "csv", about = "show csv and convert csv to other")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    pub input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+pub fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if std::path::Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("file not exist")
+    }
+}
