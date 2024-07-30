07d3b129cb88d3b5002638e589f2f566f8f61138
prosperousLi <lirongfan@vrvmail.com.cn>
2024-07-30 17:12:41 +0800

    feature:add zxcvbn test password strength

---
+++ src/process/csv_convert.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::fs;
+
+use crate::opts::OutputFormat;
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player {
+    //#[serde(rename = "Name")]
+    name: String,
+    //#[serde(rename = "Position")]
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    //#[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+}
+
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
+    let mut reader = Reader::from_path(input)?;
+    let mut ret = Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        println!("{:?}", record);
+        ret.push(json_value); // borrowd
+    }
+
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+        //OutputFormat::Toml => toml::to_string(&ret)?,
+    };
+    //let json = serde_json::to_string_pretty(&ret)?;
+    println!("out path:{:?}", output);
+    fs::write(output, content)?;
+    Ok(())
+}
---
+++ src/process/gen_pass.rs
@@ -0,0 +1,51 @@
+use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
+
+const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
+const LOWER: &[u8] = b"abcdefghijkmnpqrstuvwxyz";
+const NUMBER: &[u8] = b"123456789";
+const SYMBOL: &[u8] = b"!@#$%^&*_";
+
+pub fn process_genpass(
+    length: u8,
+    upper: bool,
+    lower: bool,
+    number: bool,
+    symbol: bool,
+) -> anyhow::Result<()> {
+    let mut rng = rand::thread_rng();
+    let mut password = Vec::new();
+    let mut chars = Vec::new();
+
+    if upper {
+        chars.extend_from_slice(UPPER);
+        password.push(*UPPER.choose(&mut rng).expect("not empty"));
+    }
+    if lower {
+        chars.extend_from_slice(LOWER);
+        password.push(*LOWER.choose(&mut rng).expect("not empty"));
+    }
+    if number {
+        chars.extend_from_slice(NUMBER);
+        password.push(*NUMBER.choose(&mut rng).expect("not empty"));
+    }
+    if symbol {
+        chars.extend_from_slice(SYMBOL);
+        password.push(*SYMBOL.choose(&mut rng).expect("not empty"));
+    }
+
+    for _ in 0..length - password.len() as u8 {
+        password.push(*chars.choose(&mut rng).expect("not empty"));
+    }
+
+    password.shuffle(&mut rng);
+
+    let password = String::from_utf8(password)?;
+
+    println!("{:?}", password);
+
+    let estimate = zxcvbn(&password, &[]);
+    eprintln!("strength:{}", estimate.score()); // 3
+
+    Ok(())
+}
